34b9b4b17cc5e4e157dbd51f1cb9cb755ca8da3d
Kalpana Paulraj <Kalpana.paulrajt@gmail.com>
2023-11-21 15:45:27 -0600

    pom.xml updated for surefire plugin config

---
+++ src/test/java/scripts/Simpletest.java
@@ -0,0 +1,61 @@
+package scripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Simpletest {
+	public static final String path = "C:/Kalpana/Installable/Drivers/chromedriver.exe";
+	public static WebDriver driver; 
+	public String mergeText="Clone Simple Test";
+	@BeforeTest
+	public void doThisBefore(){
+	    ChromeOptions option = new ChromeOptions();
+	    option.addArguments("--remote-allow-origins=*");
+	    System.setProperty("webdriver.chrome.driver", path);
+	    //System.setProperty("webdriver.http.factory", "jdk-http-client");
+	    driver = new ChromeDriver(option);
+	    driver.get("https://selenium.dev/");
+	}
+	
+    @Test(priority=1)
+    public void getTitle(){
+    String title = driver.getTitle();
+    System.out.println(driver.getTitle());
+    Assert.assertEquals(title,"Selenium");
+}
+    @Test(priority=2)
+    public void checkMenuitem1() {
+    	String mItem1=driver.findElement(By.xpath("//*[@id='navbarDropdown']")).getText();
+    	Assert.assertEquals(mItem1,"About");
+    }
+
+    @Test(priority=3)
+    public void checkbodyMsg1() {
+    	String bMsg=driver.findElement(By.xpath("/html/body/div/main/section[1]/div/div/div/h1")).getText();
+    	System.out.println(bMsg+"!");
+    	Assert.assertEquals(bMsg,"Selenium automates browsers. That's it!");
+    }
+	@Test(priority=4)
+	public void checkMenuitem1Dup() {
+    	String mItem1=driver.findElement(By.xpath("//*[@id='navbarDropdown']")).getText();
+    	Assert.assertEquals(mItem1,"About");
+		System.out.println(mergeText);
+		System.out.println("kalpana");
+		}
+
+    
+    @AfterTest
+    public void cleanMethod() {
+    	System.out.println("Browser Closed");
+    	driver.quit();
+    }
+   
+    
+}
+
